a9d6487d0b8dce2b61ad51dfec2ac86f467a9b09
Liam Farrell <liam.farrell@aerian.com>
2017-12-22 11:16:34 +0000

    initial population of static information and project structure.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,27 +1,57 @@
 import * as React from 'react';
 import './App.css';
-import { Button, Grid, Row, Col } from 'react-bootstrap';
+import { Grid, Row, Col, Image } from 'react-bootstrap';
 
-const logo = require('../static/images/logo.svg');
+const headshot = require('../static/images/headshot.png');
+// const logo = require('../static/images/logo.svg');
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Website</h1>
-        </header>
         <Grid>
+          <Row className="App-header">
+            <Col md={4} mdOffset={4} xs={4} xsOffset={4}>
+              <Image src={headshot} thumbnail responsive className="headshot"/>
+            </Col>
+          </Row>
           <Row>
-            <Col xs={12} md={8}>asd</Col>
-            <Col xs={6} md={4}>123</Col>
+            <Col><h1>Liam Farrell</h1></Col>
+          </Row>
+          <Row>
+            <Col md={3} sm={4}>
+              <a href="mailto:liam@awaitingimage.com">
+                <i className="fa fa-envelope pad-right-5" aria-hidden="true"/>liam@awaitingimage.com
+              </a>
+            </Col>
+            <Col md={3} sm={4} mdOffset={2}>
+              <a href="https://github.com/awaitingimage">
+                <i className="fa fa-github pad-right-5" aria-hidden="true"/>@awaitingimage
+              </a>
+            </Col>
+            <Col md={3} sm={4}>
+              <a href="https://www.linkedin.com/in/liam-farrell-1a513b26"> 
+                <i className="fa fa-linkedin-square pad-right-5" aria-hidden="true"/>@liamfarrell
+              </a>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <p className="summary">A high-performing Technical Scrum Master with a passion for solving complex 
+                problems by utilising mobile technology and open source software to create intuitive
+                and innovative solutions. Certified Scrum Master and experienced mobile developer for
+                IOS and Android using hybrid and native technology. Breadth of experience working with
+                both technical and non-technical colleagues to capture business requirements for product
+                design, development and testing. Leads by example, setting and adhering to high documentation
+                and coding standards, using the Agile working methodology.
+              </p>
+            </Col>
           </Row>
         </Grid>
         <p className="App-intro">
-          Awaiting Image is currently under construction.
+          Currently under construction...
         </p>
-        <Button bsStyle="primary">Primary</Button>
+        {/* <Button bsStyle="primary">Primary</Button> */}
       </div>
     );
   }
