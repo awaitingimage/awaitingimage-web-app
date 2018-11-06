355388f57ef4e096b5ae27370c93b3be521c7dd0
Liam <liam@awaitingimage.com>
2018-11-06 16:36:38 +0000

    added contact info to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { Image } from 'react-bootstrap'
 import { CSSTransition } from 'react-transition-group';
+import { Grid, Row, Col, Label } from 'react-bootstrap'
 
 import './Header.css'
 const headshot = require('../../static/images/headshot.png')
@@ -24,7 +25,7 @@ export default class Header extends React.Component {
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
     this.setState({
-      shrink: scrollTop > 20 ? true : false
+      shrink: scrollTop > 100 ? true : false
     });
   }
 
@@ -37,6 +38,29 @@ export default class Header extends React.Component {
           classNames="square"
         >
       <div className={'square'}><Image src={headshot} className={'headshot'} /></div>
+
+              
+
+      </CSSTransition>
+      <CSSTransition
+        in={this.state.shrink}
+          timeout={100}
+          classNames="info"
+          unmountOnExit
+        >
+        <div className={'info'}>
+        <h3 className={'name'}>Liam Farrell</h3>
+        <a href='mailto:liam@awaitingimage.com' className='pad-right-15'>
+          <i className='fa fa-envelope pad-right-5' aria-hidden='true' />liam@awaitingimage.com
+        </a>
+        <br/>
+        <a href='https://github.com/awaitingimage' className='pad-right-15'>
+          <i className='fa fa-github pad-right-5' aria-hidden='true' />@awaitingimage
+        </a>
+        <a href='https://www.linkedin.com/in/liam-farrell-1a513b26' className='pad-right-15'>
+          <i className='fa fa-linkedin-square pad-right-5' aria-hidden='true' />@liamfarrell
+        </a>
+        </div>
       </CSSTransition>
       </div>
     )
